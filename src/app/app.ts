47f52ce82164ad85cfabe2cb47d5d3b5d4b003b4
Gainus <adam.gainus@gmail.com>
2026-06-09 01:43:28 -0600

    building the portfolio website. including adding own logo and picture.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Introduction } from './components/introduction/introduction';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, Introduction],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('my-portfolio');
